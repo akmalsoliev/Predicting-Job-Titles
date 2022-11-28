73edb4a7de12eab8a07f31ec13fec2770514fbac
Akmal Soliev <soliev@gmail.com>
2022-11-28 20:53:46 +0100

    Minor changes

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,2 +1,3 @@
 from .model import model
 from .metrics import RMSE 
+from .k_fold import KFold
--- src/model/model.py
+++ src/model/model.py
@@ -8,16 +8,17 @@ def model():
     activation_out = tf.keras.activations.linear
 
     inputs = keras.Input(shape=(72,))
-    dense = layers.Dense(64, activation="relu", name="64NodeLayer")(inputs)
 
-    class SecondaryLayer(keras.Model):
+    # This is not required, done for possible implementation in future. 
+    class DenseLayer(keras.Model):
         def __init__(self):
             super().__init__()
-            self.dense = layers.Dense(32, activation="relu")
+            self.dense = layers.Dense(32, activation=activation_relu)
         def call(self, input):
             return self.dense(inputs)
 
-    dense2 = SecondaryLayer()(dense)
+    dense = DenseLayer()(inputs)
+    dense2 = layers.Dense(64, activation="relu", name="64NodeLayer")(dense)
     output = layers.Dense(1, activation="linear", name="Output/Prediction")(dense2)
     model = keras.Model(inputs=inputs, outputs=output, name="Basic Regression Model")
     return model 
