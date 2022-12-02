62830d738e52be0a8645c4eeeca7808720d89b19
Akmal Soliev <soliev@gmail.com>
2022-12-02 17:06:22 +0100

    Minor modifications to the files
    `main.py` is just a debugging file at this stage.

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import numpy as np
 import pandas as pd 
-# import tensorflow as tf
-# from tensorflow import keras
-from src.processing import DataPrep, train_split, Scaler
+import tensorflow as tf
+from tensorflow import keras
+from src.processing import DataPrep, Scaler
+from src import model, KFold
 
 
 def main():
     ds = DataPrep("data/jobss.csv")
-    ds.get_df_report("preprocessed")
+    #ds.get_df_report("preprocessed")
     ds()
-    ds.get_df_report("processed")
+    #ds.get_df_report("processed")
     ds.processed_export()
     df = ds.df
 
     target_col = "sal"
-    (train_data, train_labels), (test_data, test_labels) = train_split(df, target_col)
+    kfold = KFold(n_folds=4, model=model, optimizer="adam", loss="mae", metrics=["mae"])
+    results = kfold(Scaler, df, target_col, verbose=False)
+    print("The mean is follow:", df[target_col].mean())
+    print("The std is follow:", df[target_col].std())
+    print(results)
 
-    scale_cols = ["Longitude", "Latitude"]
-    standard_scaler = Scaler(train_data[scale_cols], test_data[scale_cols])
-    train_data[scale_cols] = standard_scaler.train_scale()
-    test_data[scale_cols] = standard_scaler.test_scale()
 
 if __name__ == "__main__":
     main()
