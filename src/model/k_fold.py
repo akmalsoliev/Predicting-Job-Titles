8fee6ce82be6b4b989ccb0b970e1971e5f3d5441
Akmal Soliev <soliev@gmail.com>
2022-12-02 17:05:20 +0100

    The code works fine, however, now need to build out a better model

--- src/model/k_fold.py
+++ src/model/k_fold.py
@@ -1,7 +1,9 @@
-from typing import Callable, Union, Type
-from numpy._typing import NDArray
-import pandas as pd
+from typing import Callable, Union
+
 import numpy as np
+import pandas as pd
+from numpy._typing import NDArray
+
 from ..processing import Scaler
 
 
@@ -20,8 +22,7 @@ class KFold:
     def _compile_model_(self):
         self.model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
 
-    def call(self, scaler:Scaler, data:Union[pd.DataFrame, NDArray], target:Union[str, int], epochs:int=100, **kwargs):
-
+    def __call__(self, scaler:Scaler, data:Union[pd.DataFrame, NDArray], target:Union[str, int], epochs:int=100, **kwargs):
         if isinstance(target, str) and isinstance(data, pd.DataFrame):
             self.target_index: int = np.where(data.columns==target)[0][0]
         elif isinstance(target, int) and isinstance(data, NDArray):
@@ -29,24 +30,30 @@ class KFold:
         else:
             raise TypeError("Miss-match in data and target type.")
 
-        split_data = np.array_split(data, self.n_folds)
+        split_data = np.array_split(data.to_numpy(), self.n_folds)
+        print(split_data[0])
+        split_array: NDArray = np.array(split_data)
         metrics_list: list[float] = []
 
         for fold in range(self.n_folds):
             train_index = [index for index in range(self.n_folds) if index != fold]
-            train_ds = np.concatenate(split_data[train_index], axis=0)
-            test_ds = split_data[fold]
+            train_ds: NDArray = np.vstack(split_array[train_index])
+            test_ds: NDArray = split_array[fold]
+            print("Hello World!")
             
             (train_data, train_labels) = self.__data_label_split__(train_ds)
+            print(type(test_ds))
             (test_data, test_labels) = self.__data_label_split__(test_ds) 
 
             # This is an extremely lazy of implementing scaling, just applying scaling 
-            # on the hole dataset, not recommended, but it won't effect my data as
+            # on the hole dataset, not recommended, but it won't affect my data as
             # except lat and long is hot-encoded.
             standard_scaler = scaler(train_data, test_data)
             train_data = standard_scaler.train_scale()
             test_data = standard_scaler.test_scale()
 
-            self.model.fit(train_data, train_labels, epochs=epochs, kwargs=kwargs)
+            self.model.fit(train_data, train_labels, epochs=epochs, **kwargs)
 
             metrics_list.append(self.model.evaluate(test_data, test_labels))
+        
+        return metrics_list
